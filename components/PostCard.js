0df9a070b143cb573038f266f02ca06bea7b168c
yamayamaKo <yamachanwow@gmail.com>
2021-03-17 18:45:18 +0900

    add:都道府県ごとの投稿を表示できるようにした

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,38 +1,52 @@
+import { useEffect, useState } from "react";
+import firebase from 'firebase'
 
 const PostCard = (props) => {
-    // TODO:propsで引数を与えて、指定した画像を表示できるようにする
+    const [imgSrc, setImgSrc] = useState("../static/images/tizu.png")
+    const [title, setTitle] = useState("デフォルトタイトル")
+    const [author, setAuthor] = useState("デフォルト著者")
+    const postId = props.postId
+    const db = firebase.database()
+
+    useEffect(()=>{
+        const postRef = db.ref('posts/'+postId)
+        postRef.on('value',(snapshot)=>{
+            setTitle(snapshot.val().title)
+            setAuthor(snapshot.val().user_name)
+            firebase.storage().ref(snapshot.val().img[0]).getDownloadURL().then((url)=>{
+                setImgSrc(url);
+            })
+        })
+    },[])
+
     return(
         <div className='w-80'>
             <article className="overflow-hidden rounded-lg shadow-lg">
 
                 <a href="#">
-                    <img alt="Placeholder" className="block h-64 w-full" src="./static/images/tizu.png" style={{objectFit:"contain"}}/>
+                    <img alt="Placeholder" className="block h-64 w-full" src={imgSrc} style={{objectFit:"contain"}}/>
                 </a>
 
                 <div className='h-40'>
                     <header className="flex-wrap items-center justify-between leading-tight p-2 md:p-4">
                         <h1 className="text-lg">
                             <a className="no-underline hover:underline text-black" href="#">
-                                日本一周の旅だった
+                                {title}
                             </a>
                         </h1>
-
+{/* 
                         <p className="text-grey-darker text-sm">
                             Date
-                        </p>
+                        </p> */}
                     </header>
 
                     <footer className="flex items-center justify-between leading-none p-2 md:p-4">
                         <a className="flex items-center no-underline hover:underline text-black" href="#">
                             {/* <img alt="Placeholder" className="block rounded-full" src="../static/images/train.png" style={{objectFit:"contain"}}/> */}
                             <p className="text-sm">
-                                Author Name
+                                {author}
                             </p>
                         </a>
-                        {/* <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                            <span className="hidden">Like</span>
-                            <i className="fa fa-heart"></i>
-                        </a> */}
                     </footer>
                 </div>
             </article>
