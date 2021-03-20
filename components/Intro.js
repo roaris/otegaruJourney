3310a57ec4b982d98a185680e4fe8a500e6073c6
roaris <one0803kyopro@gmail.com>
2021-03-21 07:26:32 +0900

    背景画像の変更

--- components/Intro.js
+++ components/Intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
     // }
 
     const bg_random = {
-        backgroundImage: 'url(../static/images/introbokashi.jpg)',
+        backgroundImage: 'url(../static/images/header_back.png)',
         backdropFilter: 'blur(8px)'
     }
 
@@ -19,11 +19,6 @@ const Intro = () => {
         // className="min-w-screen min-h-screen max-w-full p-5 container flex flex-col justify-center items-center
         // bg-no-repeat bg-cover bg-center">
         <div style={bg_random} className='header-background'>
-            {/* <h1>お手軽Jorney</h1>
-            <h2>お手軽Jorneyは旅行の記録を投稿したり、<br/>他の人の旅行の記録を楽しむこと<br/>の出来るアプリケーションです。</h2>
-            <Link href='#map'>
-            <button type="button" className="focus:outline-none font-bold py-2 px-2 rounded-md border-2 border-white hover:bg-gray-200 hover:border-gray-200">さっそく楽しむ</button>
-            </Link> */}
             {/* <div className='header-title'>
                 <div className='text-white font-bold'>
                     <h1>お手軽Jorney</h1>
